f336fedfbd6ed85fe42118f0b880e251cd1717a9
Hadeer elnaghy <hadeerelnaghy2@gmail.com>
2026-09-02 14:51:39 +0300

    Update start.sh

--- scripts/start.sh
+++ scripts/start.sh
@@ -1,13 +1,10 @@
 #!/bin/bash
+set -e
+export PATH=$PATH:/usr/local/bin:/usr/bin
 
-# Go to the application folder
 cd /home/ubuntu/myapp
-
-# Install dependencies from package.json
 npm install
 
-# Check if the server is marked as Production or Dev
-# We will set this ENV_NAME variable on the server itself later
 if [ "$ENV_NAME" == "prod" ]; then
     echo "Applying Production settings..."
     cp config-prod.json config.json
@@ -16,6 +13,6 @@ else
     cp config-dev.json config.json
 fi
 
-# Stop any old running app, then start the new one using PM2
 pm2 stop server || true
+pm2 delete server || true
 pm2 start server.js --name "my-web-app"
