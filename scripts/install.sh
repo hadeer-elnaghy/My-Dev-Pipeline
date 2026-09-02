81173deabd0b9bf5a1f5f43cf06a497c441b0f19
Hadeer elnaghy <hadeerelnaghy2@gmail.com>
2026-09-02 14:51:05 +0300

    Update install.sh

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,10 +1,6 @@
 #!/bin/bash
+set -e
 
-# Update server packages
 apt-get update -y
-
-# Install Node.js and npm
 apt-get install -y nodejs npm
-
-# Install PM2 globally to keep the app running in the background
-npm install pm2 -g1~a#!/bin/bash
+npm install -g pm2
